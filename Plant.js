fc62d0de793f31f6bff4f9aee6033fa3b0689213
Kirill Stahl <13516176@student.uts.edu.au>
2023-05-04 20:42:48 +1000

    L-System Plant added
    - L-System plant added
    - grassShaderInit() and grassShaderPlane() added
    - Refactoring

--- Plant.js
+++ Plant.js
@@ -23,6 +23,11 @@ var backgroundColour;
 
 let plane;
 
+//shaders
+let vertexShader, fragmentShader, uniforms, leavesMaterial;
+//deltaTime
+const clock = new THREE.Clock();
+
 //create the scene
 scene = new THREE.Scene();
 ratio = window.innerWidth/window.innerHeight;
@@ -56,12 +61,20 @@ var controls = new OrbitControls(camera, renderer.domElement );
 //========DEBUG===========
 initLights();
 loadSkybox();
-//loadTestSphere();
+initGrassShader();
+initGrassPlane();
+lindenmayerPlant();
 loadBaseGroundModel();
 loadLizard();
 renderGui();
-//animate(); //is placed at the bottom of code for get grass shader working 
+animate(); 
+//is placed at the bottom of code for get grass shader working. 
+//Edit: I fixed it be declaring shader variables and splitting your code into initGrassShader() and initGrassPlane() at the top of the document;
 //========================
+
+//Event Listeners
+window.addEventListener('resize', onWindowResize);
+window.addEventListener('click', () => {PlayAudio()}, {once: true});
 
 //Handle window resize
 function onWindowResize() 
@@ -73,8 +86,6 @@ function onWindowResize()
   controls.handleResize();
 }
 
-window.addEventListener('resize', onWindowResize);
-
 //Lighting
 function initLights(){
   ambLight = new THREE.AmbientLight(ambLightColour, ambLightInten);
@@ -85,6 +96,7 @@ function initLights(){
   scene.add(dirLight);
 }
 
+//Skybox
 function loadSkybox(){
   //adding textures 
   const cubeTextureLoader = new THREE.CubeTextureLoader();
@@ -98,31 +110,254 @@ function loadSkybox(){
   ])
 }
 
-function loadTestSphere(){
-  const planeGeometry = new THREE.PlaneGeometry(32, 32);
-  const planeMaterial = new THREE.MeshLambertMaterial({
-    color: 0xFFFFF,
+//Grass Shader
+function initGrassShader(){
+
+  vertexShader = `
+    varying vec2 vUv;
+    uniform float time;
+    
+    void main() {
+
+      vUv = uv;
+      
+      // VERTEX POSITION
+      
+      vec4 mvPosition = vec4(position, 1.0);
+      #ifdef USE_INSTANCING
+        mvPosition = instanceMatrix * mvPosition;
+      #endif
+      
+      // DISPLACEMENT
+      
+      // here the displacement is made stronger on the blades tips.
+      float dispPower = 1.0 - cos( uv.y * 3.1416 / 2.0 );
+      
+      float displacement = sin( mvPosition.z + time * 10.0 ) * ( 0.1 * dispPower );
+      mvPosition.z += displacement;
+      
+      //
+      
+      vec4 modelViewPosition = modelViewMatrix * mvPosition;
+      gl_Position = projectionMatrix * modelViewPosition;
+
+    }
+  `;
+
+  fragmentShader = `
+    varying vec2 vUv;
+    
+    void main() {
+      vec3 baseColor = vec3(1, 0.2, 0.7); // was 0.41, 1.0, 0.5 
+      float clarity = (vUv.y * 0.5 ) + 0.5;
+      gl_FragColor = vec4(baseColor * clarity, 1 );
+    }
+  `;
+
+  uniforms = {
+    time: {
+      value: 0
+    }
+  }
+
+  leavesMaterial = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms,
     side: THREE.DoubleSide
   });
-
-plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = -0.5 * Math.PI;
-//Plane.castShadow = true;
-plane.recieveShadow = true;
-scene.add(plane);
-
-const sphereGeometry = new THREE.SphereGeometry(5, 32, 32);
-const sphereMaterial = new THREE.MeshPhongMaterial({color: 0x0000FF, /*texture for sphere */ //map: texture
-})
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.castShadow = true;
-//Sphere.recieveShadow = true;
-sphere.position.set(-4,14,-1);
-
-scene.add(sphere);
 }
 
-//Lizard decoration
+//Grass Plane
+function initGrassPlane(){
+  const instanceNumber = 500; //was 5000
+  const grass = new THREE.Object3D();
+
+  const geometry = new THREE.PlaneGeometry(0.1, 1, 1, 4);// was 0.1, 1, 1, 4
+  geometry.translate(0, 3, 0); // move grass blade geometry lowest point at 0. // original position is 0, 0.5, 0
+
+  const instancedMesh = new THREE.InstancedMesh(geometry, leavesMaterial, instanceNumber);
+
+  scene.add(instancedMesh);
+
+  //Position and scale the grass blade instances randomly.
+
+  for (let i = 0 ; i < instanceNumber; i++) {
+
+    grass.position.set(
+      ( Math.random() - 0.5 ) * 40, //original rand is ( Math.random() - 0.5 ) * 10, 0, ( Math.random() - 0.5 ) * 10
+      0,
+      ( Math.random() - 0.5 ) * 40
+    ); 
+
+    grass.userData.name = "grassPlane";
+    grass.scale.setScalar(0.5 + Math.random() * 0.5);
+    grass.rotation.y = Math.random() * Math.PI;
+    grass.updateMatrix();
+    instancedMesh.setMatrixAt(i, grass.matrix);
+  }
+}
+
+//L-System Plant
+function lindenmayerPlant(){
+  //https://codepen.io/mikkamikka/pen/DrdzVK
+
+  function Params() {
+    this.iterations = 2;
+    this.theta = 18;
+    this.thetaRandomness = 100;
+    this.angle = 0;
+    this.scale = 4;
+    this.scaleRandomness = 100;
+    this.constantWidth = true;
+    this.deltarota = 30;
+  }
+
+  function Rules()  {
+    this.axiom = 'F';
+    this.mainRule = 'FF-[-F+F+F]+[+F-F-F]';
+    this.Rule2 = '';
+  }
+
+  var rules = new Rules();
+  var params = new Params();
+
+  var clear = {clear: function()
+    {canvas.width = canvas.width;}};
+
+  function GetAxiomTree() {
+    var Waxiom = rules.axiom;
+    var newf = rules.mainRule;
+    var newb = 'bb';
+    var newx = rules.Rule2;
+    var level = params.iterations;    
+    while (level > 0) {        
+        var m = Waxiom.length;
+        var T = '';        
+        for (var j=0; j < m; j++) {
+            var a = Waxiom[j];
+            if (a == 'F'){T += newf;}
+            else
+              if (a == 'b'){T += newb;}
+              else                   
+                if (a == 'X'){T += newx;}
+                else
+                  T += a;
+        }
+        Waxiom = T;
+        level--;
+    }
+    return Waxiom;
+  }
+
+  function DrawTheTree(plantGeometry, x_init, y_init, z_init){   
+    let plantVertices = plantGeometry;
+    var Wrule = GetAxiomTree();
+    var n = Wrule.length;
+    var stackX = []; var stackY = [];  var stackZ = []; var stackA = [];
+    var stackV = []; var stackAxis = [];
+
+    var theta = params.theta * Math.PI / 180; 
+    var scale = params.scale;
+    var angle = params.angle * Math.PI / 180;
+
+    var x0 = x_init; var y0 = y_init; var z0 = z_init;
+    var x; var y; var z;  
+    var rota = 0, rota2 = 0,
+        deltarota = 18 * Math.PI/180;  
+    var newbranch = false;
+    var axis_x = new THREE.Vector3( 1, 0, 0 );
+    var axis_y = new THREE.Vector3( 0, 1, 0 );
+    var axis_z = new THREE.Vector3( 0, 0, 1 );
+    var zero = new THREE.Vector3( 0, 0, 0 );
+    var axis_delta = new THREE.Vector3(),
+      prev_startpoint = new THREE.Vector3();
+
+    var startpoint = new THREE.Vector3(x0,y0,z0), 
+        endpoint = new THREE.Vector3();
+    var bush_mark;
+    var vector_delta = new THREE.Vector3(scale, scale, 0);
+
+    for (var j=0; j<n; j++){        
+        var a = Wrule[j];
+        if (a == "+")
+        {
+          angle -= theta;                     
+        }
+        if (a == "-")
+        {
+          angle += theta;                                 
+        }
+        if (a == "F"){
+          var a = vector_delta.clone().applyAxisAngle( axis_y, angle );          
+          endpoint.addVectors(startpoint, a);  
+          
+          plantVertices.push(startpoint.clone());
+          plantVertices.push(endpoint.clone());
+
+          prev_startpoint.copy(startpoint);
+          startpoint.copy(endpoint);
+          axis_delta = new THREE.Vector3().copy(a).normalize();
+          rota += deltarota;// + (5.0 - Math.random()*10.0);
+          
+        } 
+        if (a == "L")
+        {
+          endpoint.copy(startpoint);
+          endpoint.add(new THREE.Vector3(0, scale*1.5, 0));
+          var vector_delta2 = new THREE.Vector3().subVectors(endpoint, startpoint);
+          vector_delta2.applyAxisAngle( axis_delta, rota2 );
+          endpoint.addVectors(startpoint, vector_delta2); 
+          
+          plantVertices.push(startpoint.clone());
+          plantVertices.push(endpoint.clone());          
+
+          rota2 += 45 * Math.PI/180;
+        }
+        if (a == "%"){}
+        if (a == "["){
+            stackV.push(new THREE.Vector3(startpoint.x, startpoint.y, startpoint.z));            
+            stackA[stackA.length] = angle;            
+        }
+        if (a == "]"){
+            var point = stackV.pop();
+            startpoint.copy(new THREE.Vector3(point.x, point.y, point.z));
+            angle = stackA.pop();
+        }        
+      bush_mark = a;
+    }
+  return plantVertices;
+  }
+
+  function setRules(){
+    rules.axiom = "F";
+    rules.mainRule = "F-F[-F+F[LLLLLLLL]]++F[+F[LLLLLLLL]]--F[+F[LLLLLLLL]]";
+    params.iterations = 3;
+    params.angle = 0;
+    params.theta = 30;
+    params.scale = 6;    
+  }
+
+  function plantInit() {
+
+  setRules();
+
+  let plantGeometry = [];
+  var material = new THREE.LineBasicMaterial({color: 0xA91B60}); //pink
+  material.linewidth = 3;
+  plantGeometry = DrawTheTree(plantGeometry, 0, -150, 0);  
+  var geometry = new THREE.BufferGeometry().setFromPoints(plantGeometry);
+
+  //plant = new THREE.Mesh(line_geometry, material);
+  var plant = new THREE.Line(geometry, material);
+  plant.position.set(8,7,-6);
+  plant.scale.setScalar(0.05);
+  scene.add(plant);       
+  }
+  plantInit();
+}
+
+//Pink Lizard
 function loadLizard(){
   const fbxLoader = new FBXLoader();
   fbxLoader.setResourcePath("./textures/pink_lizard/");
@@ -142,13 +377,10 @@ function loadLizard(){
   lizard.position.set(-10, 2.2, -10);
   lizard.rotation.set(-190, 0, 90);
   scene.add(lizard);
-});
+  });
 }
 
-// --- Sounds ---
-//start when clicked once
-window.addEventListener('click', () => {PlayAudio()}, {once: true});
-
+//Music
 function PlayAudio(){
 const listener = new THREE.AudioListener();
 //laod audio file 
@@ -186,111 +418,12 @@ function loadBaseGroundModel(){
     });
 }
 
-  const WIDTH = window.innerWidth;
-  const HEIGHT = window.innerHeight;
-  //skip camera, scene, renderer
-  //we already have control 
-  const clock = new THREE.Clock();
-
-  const vertexShader = `
-    varying vec2 vUv;
-    uniform float time;
-    
-    void main() {
-
-      vUv = uv;
-      
-      // VERTEX POSITION
-      
-      vec4 mvPosition = vec4(position, 1.0);
-      #ifdef USE_INSTANCING
-        mvPosition = instanceMatrix * mvPosition;
-      #endif
-      
-      // DISPLACEMENT
-      
-      // here the displacement is made stronger on the blades tips.
-      float dispPower = 1.0 - cos( uv.y * 3.1416 / 2.0 );
-      
-      float displacement = sin( mvPosition.z + time * 10.0 ) * ( 0.1 * dispPower );
-      mvPosition.z += displacement;
-      
-      //
-      
-      vec4 modelViewPosition = modelViewMatrix * mvPosition;
-      gl_Position = projectionMatrix * modelViewPosition;
-
-    }
-  `;
-
-  const fragmentShader = `
-    varying vec2 vUv;
-    
-    void main() {
-      vec3 baseColor = vec3(1, 0.2, 0.7); // was 0.41, 1.0, 0.5 
-      float clarity = (vUv.y * 0.5 ) + 0.5;
-      gl_FragColor = vec4(baseColor * clarity, 1 );
-    }
-  `;
-
-  const uniforms = {
-    time: {
-      value: 0
-    }
-  }
-
-  const leavesMaterial = new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    uniforms,
-    side: THREE.DoubleSide
-  });
-
-  /////////
-  // MESH
-  /////////
-
-  const instanceNumber = 500; //was 5000
-  const dummy = new THREE.Object3D();
-
-  const geometry = new THREE.PlaneGeometry(0.1, 1, 1, 4);// was 0.1, 1, 1, 4
-  geometry.translate(0, 3, 0); // move grass blade geometry lowest point at 0. // original position is 0, 0.5, 0
-
-  const instancedMesh = new THREE.InstancedMesh(geometry, leavesMaterial, instanceNumber);
-
-  scene.add(instancedMesh);
-
-  // Position and scale the grass blade instances randomly.
-
-  for (let i = 0 ; i < instanceNumber; i++) {
-
-    dummy.position.set(
-      ( Math.random() - 0.5 ) * 40, //original rane is ( Math.random() - 0.5 ) * 10, 0, ( Math.random() - 0.5 ) * 10
-      0,
-      ( Math.random() - 0.5 ) * 40
-    ); 
-
-    dummy.scale.setScalar(0.5 + Math.random() * 0.5);
-    dummy.rotation.y = Math.random() * Math.PI;
-    dummy.updateMatrix();
-    instancedMesh.setMatrixAt(i, dummy.matrix);
-    }
-
 //Dat GUI
 function renderGui()
 {
   const gui = new GUI();
 
   //parameters for GUI
-  //Test shere
-  let options = {
-    sphereColor: '#ffea00',
-    angle: 0.2,
-    penumbra: 0,
-    intensity: 1,
-  }
-
-
 //colour variables
   let col = {
     ambLightColour: 0xe52b50,  //dark pink - Amaranth shade
@@ -301,7 +434,6 @@ function renderGui()
     plantSecondColour: 0xffffff, //white
     plantThirdColour: 0xffffff //white
   }
-
 
   let colourFolder = gui.addFolder("Scene Colour Management");
 
@@ -334,23 +466,12 @@ function renderGui()
   //colourFolder.add(mesh.rotation, "y", 0, Math.PI * 2, 0.001).name("Secondary Clour");
   //colourFolder.add(mesh.rotation, "z", 0, Math.PI * 2, 0.001).name("Accent Colour");
   //colourFolder.add(mesh.rotation, "z", 0, Math.PI * 2, 0.001).name("Regenerate");
-
-  //let SphereFolder = gui.addFolder("Sphere Management");
-  //SphereFolder.addColor(options, 'sphereColor').onChange(function(e){
-    //Sphere.material.color.set(e)
-  //});
-
-  //below three gui options are for the light
-  let lightFolder = gui.addFolder("Light Attributes Management");
-  lightFolder.add(options, 'angle', 0, 1)
-  lightFolder.add(options, 'penumbra', 0, 1)
-  lightFolder.add(options, 'intensity', 0, 1)
 }
 
+//Animate
 function animate(){
   requestAnimationFrame(animate);
   render();
-
   //grass shader animation
   // Hand a time variable to vertex shader for wind displacement.
 	leavesMaterial.uniforms.time.value = clock.getElapsedTime();
@@ -361,8 +482,6 @@ function animate(){
   controls.update();
 
 }
-
-animate();
 
 function render() 
 {
